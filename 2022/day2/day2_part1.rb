f562aa1c6f3c6383629615ae47cc2fc8056788ae
Kyle Soskin <kyle.soskin@adhocteam.us>
2022-12-02 09:07:37 -0500

    Further mining

--- 2022/day2/day2_part1.rb
+++ 2022/day2/day2_part1.rb
@@ -16,8 +16,7 @@ ENC = {
 
 puts File.read('input.txt').lines.map {|x| 
   p1, p2 =  x.split
-  p1 = ENC[p1]
-  (p1 == p2 ? 3 : (BEATS[p2] == p1 ? 6 : 0)) + SCORES[p2]
+  ((p1 = ENC[p1]) == p2 ? 3 : (BEATS[p2] == p1 ? 6 : 0)) + SCORES[p2]
 }.sum
 # Result: 17189
 
