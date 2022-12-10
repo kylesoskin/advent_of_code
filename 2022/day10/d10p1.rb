67c7a108a9bd139b80227ddec8de8f14274c9dce
Kyle Soskin <kyle.soskin@adhocteam.us>
2022-12-10 09:37:45 -0500

    d10p1

---
+++ 2022/day10/d10p1.rb
@@ -0,0 +1,32 @@
+input = File.read('input.txt').lines.map(&:chomp)
+
+def signal_strength(cycle, x)
+  cycle * x
+end
+
+cycle_count = 0
+x = 1
+cycles = {}
+
+input.each do |i|
+  cmd = i.split(' ')
+  if cmd.size == 1
+    cycle_count += 1 
+    cycles[cycle_count] = x
+  else
+    cycle_count += 1
+    cycles[cycle_count] = x
+    cycle_count += 1
+    cycles[cycle_count] = x
+    x += cmd.last.to_i
+  end
+end
+index = 20
+acc = 0
+while index <= 220
+  v = signal_strength(index, cycles[index])
+  pp [index, cycles[index], v]
+  acc += v
+  index += 40
+end
+p acc
