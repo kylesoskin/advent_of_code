77df53813b85bfe1d884f79f0f49298004d59fac
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2023-12-06 07:48:51 -0500

    Slight optimizing

--- 2023/day6/1.rb
+++ 2023/day6/1.rb
@@ -1,17 +1,9 @@
-all_lines = File.readlines('input.txt').map(&:chomp)
-# all_lines = File.readlines('sample.txt').map(&:chomp)
+times, distances = File.readlines('input.txt').map(&:chomp)
+h = Hash[times.split(':').last.split(" ").map(&:to_i).zip(distances.split(':').last.split(" ").map(&:to_i))]
 
-times, distances = all_lines
-
-t = times.split(':').last.split(" ").map(&:to_i)
-d = distances.split(':').last.split(" ").map(&:to_i)
-h = Hash[t.zip(d)]
-
-pp h.map{|race_duration, record_distance|
-  pp "race duration: #{race_duration}, record_distance #{record_distance}"
-  pp (0..race_duration).map {|holding_button| 
+p h.map{|race_duration, record_distance|
+  (0..race_duration).map {|holding_button| 
     remaining_duration = race_duration - holding_button
-    total_distance = remaining_duration * holding_button
-    total_distance
+    remaining_duration * holding_button
   }.select {|distance| distance > record_distance}.count
 }.inject(&:*)
