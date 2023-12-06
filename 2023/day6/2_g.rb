55e796a632a20b67d6d7633e582e6d3441a4daf4
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2023-12-06 08:06:12 -0500

    a little shorter

--- 2023/day6/2_g.rb
+++ 2023/day6/2_g.rb
@@ -1,2 +1,2 @@
-t, d = File.readlines('input.txt').map(&:chomp)
-p {t.split(?:).last.delete(' ').to_i => d.split(?:).last.delete(' ').to_i}.map{|x, y| (0..x).map {|z| (x - z) * z}.select {|q| q > y}.size}.inject(&:*)
+x, y = File.readlines('input.txt').map{|l| l.scan(/\d/).join.to_i}
+p (0..x).map {|z| (x - z) * z}.select {|q| q > y}.size
