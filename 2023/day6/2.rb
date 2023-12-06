484a158097e1e5be8adb444a1e67649e68094f28
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2023-12-06 07:45:05 -0500

    Part 2 done

---
+++ 2023/day6/2.rb
@@ -0,0 +1,18 @@
+all_lines = File.readlines('input.txt').map(&:chomp)
+# all_lines = File.readlines('sample.txt').map(&:chomp)
+
+times, distances = all_lines
+
+t = [times.split(':').last.delete(' ').to_i]
+d = [distances.split(':').last.delete(' ').to_i]
+h = Hash[t.zip(d)]
+
+
+pp h.map{|race_duration, record_distance|
+  pp "race duration: #{race_duration}, record_distance #{record_distance}"
+  pp (0..race_duration).map {|holding_button| 
+    remaining_duration = race_duration - holding_button
+    total_distance = remaining_duration * holding_button
+    total_distance
+  }.select {|distance| distance > record_distance}.count
+}.inject(&:*)
