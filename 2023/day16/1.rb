6a25ae2821c93bdcaafde1fb82ebbcce0d998314
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2023-12-16 21:17:55 -0500

    MUCH faster

--- 2023/day16/1.rb
+++ 2023/day16/1.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
-
+require 'set'
 @data = File.readlines(
   'input.txt'
 ).map { |l| l.chomp.chars }
@@ -14,26 +14,21 @@ MIRRORS = ['/', '\\'].freeze
 SPLITTERS = ['|', '-'].freeze
 
 def move(d, x, y)
-  @travel_path[y][x] = '#'
-  # ox, oy = x,y
+  @travel_path[y][x] = ?#
+  @been_to << [d, x, y]
   case d
   when :right
     x += 1
-    # y unchanged
   when :left
     x -= 1
-    # y unchanged
   when :up
-    # x unchanged
     y -= 1
   when :down
-    # x unchanged
     y += 1
   else
     raise "Got unknown direction #{d}"
   end
   return nil if y >= @data.size || x >= @data.first.size || x.negative? || y.negative?
-
   [@data[y][x], x, y]
 end
 
@@ -101,28 +96,28 @@ elsif MIRRORS.include?(current_val)
 end
 @next_moves = [[heading, x, y]]
 @unchanged_count = 0
+@been_to = Set.new
 
-until @next_moves.empty? || @unchanged_count > 10_000
+until @next_moves.empty? 
   orig_state = @travel_path.map(&:clone)
   d, x, y = @next_moves.shift
   pnext = move(d, x, y)
   next if pnext.nil?
   current_val, x, y = pnext
   if current_val == EMPTY
-    @next_moves << [d, x, y]
+    n = [d, x, y]
+    @next_moves << n unless @been_to.include?(n)
   elsif MIRRORS.include?(current_val)
     next_direction = mirror_mapping(current_val, d)
-    @next_moves << [next_direction, x, y]
+    n = [next_direction, x, y] 
+    @next_moves << n unless @been_to.include?(n)
   elsif SPLITTERS.include?(current_val)
     next_directions = spliter_mapping(current_val, d)
     next_directions.each do |d|
-      @next_moves << [d, x, y]
+      n = [d, x, y]
+      @next_moves << n unless @been_to.include?(n)
     end
   end
-  # @travel_path[y-10..y+10].each{|r| puts r.join[x-10..x+10]}
-
-  # @travel_path.each{|r| puts r.join}
-  # puts "At #{[x,y]}. Moves remaining: #{@next_moves.count}, unchanged count: #{@unchanged_count}"
   @next_moves = @next_moves.uniq
   end_state = @travel_path.map(&:clone)
   if orig_state == end_state
@@ -130,8 +125,6 @@ until @next_moves.empty? || @unchanged_count > 10_000
   else
     @unchanged_count = 0
   end
-  # gets
 end
 
-pp @travel_path.map(&:join)
 pp @travel_path.map { |r| r.count('#') }.sum
