ffcb5c58107c6da28d1b5cee6a6ec5398e551e61
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2024-12-02 08:03:46 -0500

    Day 2 plus rubocop

--- 2024/day_02/part2.rb
+++ 2024/day_02/part2.rb
@@ -2,6 +2,7 @@
 
 # input = 'sample_input.txt'
 input = 'input.txt'
+
 levels = File.read(input).lines
 
 def increasing_or_decreasing?(n, d)
@@ -16,15 +17,26 @@ def safe?(n, d)
   increasing_or_decreasing?(n, d) && delta_range(d)
 end
 
-results = []
-levels.each do |level|
-  nums = level.chomp.split(' ').map(&:to_i)
-  delta_accumulator = []
+def less_one_combos(nums)
+  (0..nums.size - 1).map do |n|
+    new_array = nums.dup
+    new_array.delete_at(n)
+    new_array
+  end
+end
+
+def nums_are_safe?(nums, delta_accumulator = [])
   nums.each_with_index do |num, index|
     next if index == nums.size - 1
 
     delta_accumulator << nums[index + 1] - num
   end
-  results << safe?(nums, delta_accumulator)
+  safe?(nums, delta_accumulator)
 end
-pp results.count(true)
+
+final_results = 0
+levels.each do |level|
+  nums = level.chomp.split(' ').map(&:to_i)
+  final_results += 1 if nums_are_safe?(nums) || less_one_combos(nums).any? { |nums| nums_are_safe?(nums) == true }
+end
+pp final_results
