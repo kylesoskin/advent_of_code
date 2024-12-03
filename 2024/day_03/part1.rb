21fcba7d6a1d2592f20064b49ecc539839f29cf7
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2024-12-03 09:46:01 -0500

    rubocop

--- 2024/day_03/part1.rb
+++ 2024/day_03/part1.rb
@@ -1,1 +1,3 @@
-pp File.read('input.txt').scan(/mul\(\d*,\d*\)/).map {|d| d.tr('mul()','').split(?,).map(&:to_i).inject(:*)}.sum
+# frozen_string_literal: true
+
+pp File.read('input.txt').scan(/mul\(\d*,\d*\)/).map { |d| d.tr('mul()', '').split(',').map(&:to_i).inject(:*) }.sum
--- 2024/day_03/part2.rb
+++ 2024/day_03/part2.rb
@@ -1,23 +1,25 @@
+# frozen_string_literal: true
+
 input = 'input.txt'
 # input = 'sample_input2.txt'
 input = File.read(input)
 
 def sum_multiples(str)
-  str.scan(/mul\(\d*,\d*\)/).map {|d| d.tr('mul()','').split(?,).map(&:to_i).inject(:*)}.sum
+  str.scan(/mul\(\d*,\d*\)/).map { |d| d.tr('mul()', '').split(',').map(&:to_i).inject(:*) }.sum
 end
 
 sum = 0
 done = false
-until done 
+until done
   dont = input.index("don't()")
-  if dont.nil? 
+  if dont.nil?
     sum += sum_multiples(input)
     done = true
-  else 
+  else
     sum += sum_multiples(input[0..dont])
-    input = input[dont+1..-1]
+    input = input[dont + 1..]
     do_loc = input.index('do()')
-    input = input[do_loc..-1]
+    input = input[do_loc..]
   end
-end 
+end
 pp sum
