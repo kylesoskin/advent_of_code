39675ccfdb40fca6aba51c7f939de62c3cc01f24
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2024-12-03 09:19:12 -0500

    Part 2

--- 2024/day_03/part2.rb
+++ 2024/day_03/part2.rb
@@ -1,4 +1,23 @@
-# frozen_string_literal: true
-
-# input = 'sample_input.txt'
 input = 'input.txt'
+# input = 'sample_input2.txt'
+input = File.read(input)
+
+def sum_multiples(str)
+  str.scan(/mul\(\d*,\d*\)/).map {|d| d.tr('mul()','').split(?,).map(&:to_i).inject(:*)}.sum
+end
+
+sum = 0
+done = false
+until done 
+  dont = input.index("don't()")
+  if dont.nil? 
+    sum += sum_multiples(input)
+    done = true
+  else 
+    sum += sum_multiples(input[0..dont])
+    input = input[dont+1..-1]
+    do_loc = input.index('do()')
+    input = input[do_loc..-1]
+  end
+end 
+pp sum
