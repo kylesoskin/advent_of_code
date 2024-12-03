6c03b5265538b2de4e0f7ba991a02c523637d429
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2024-12-03 09:49:27 -0500

    using delete instead of tr

--- 2024/day_03/part1.rb
+++ 2024/day_03/part1.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-pp File.read('input.txt').scan(/mul\(\d*,\d*\)/).map { |d| d.tr('mul()', '').split(',').map(&:to_i).inject(:*) }.sum
+pp File.read('input.txt').scan(/mul\(\d*,\d*\)/).map { |d| d.delete('mul()').split(',').map(&:to_i).inject(:*) }.sum
