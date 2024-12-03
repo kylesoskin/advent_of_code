1f6e054022416bf31a434f859cb58c6d7b4bd5d6
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2024-12-03 07:47:45 -0500

    Part 1

---
+++ 2024/day_03/part1.rb
@@ -0,0 +1,1 @@
+pp File.read('input.txt').scan(/mul\(\d*,\d*\)/).map {|d| d.tr('mul()','').split(?,).map(&:to_i).inject(:*)}.sum
---
+++ 2024/day_03/part2.rb
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+# input = 'sample_input.txt'
+input = 'input.txt'
