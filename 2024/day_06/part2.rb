a35eb57e658467245e2bc90584ebb5b42632311d
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2024-12-06 11:56:40 -0500

    minor perf improv

--- 2024/day_06/part2.rb
+++ 2024/day_06/part2.rb
@@ -6,14 +6,6 @@ golden_input = File.read('input.txt').lines.map { |l| l.chomp.chars }
 class WalkInstance
   attr_accessor :input, :heading, :cur_row, :cur_col
 
-  def find_start
-    @input.each_with_index do |row, r|
-      row.each_with_index do |val, c|
-        return [r, c] if val == '^'
-      end
-    end
-  end
-
   def shift_90
     case @heading
     when :up
@@ -28,9 +20,7 @@ class WalkInstance
   end
 
   def move
-    next_row, next_col = next_pos
-    @cur_row = next_row
-    @cur_col = next_col
+    @cur_row, @cur_col = next_pos
     @input[@cur_row][@cur_col] = 'X'
   end
 
@@ -49,7 +39,7 @@ class WalkInstance
 
   def next_val
     next_row, next_col = next_pos
-    if next_row > @input.size - 1 || next_col > @input.first.size - 1 || next_col.negative? || next_row.negative?
+    if next_row > @input_size || next_col > @input_f_s || next_col.negative? || next_row.negative?
       return nil
     end
 
@@ -58,10 +48,9 @@ class WalkInstance
     nil
   end
 
-  def walk!
-    @cur_row, @cur_col = find_start
+  def walk!    
     steps = 0
-    until @done || steps > 100_000
+    until @done || steps > 10_000
       steps += 1
       n = next_val
       if n.nil?
@@ -75,21 +64,34 @@ class WalkInstance
     steps
   end
 
-  def initialize(x)
+  def initialize(x,start)
     @heading = :up
     @done = false
     @input = x
+    @cur_row, @cur_col = start
+    @input_size = @input.size - 1
+    @input_f_s = @input.first.size - 1
   end
 end
 results = []
+
+def find_start(input)
+  input.each_with_index do |row, r|
+    row.each_with_index do |val, c|
+      return [r, c] if val == '^'
+    end
+  end
+end
+start = find_start(golden_input)
 golden_input.each_with_index do |r, ri|
   r.each_with_index do |_c, ci|
     new_input = Marshal.load(Marshal.dump(golden_input))
     new_input[ri][ci] = '0'
-    x = WalkInstance.new(new_input)
+    x = WalkInstance.new(new_input, start)
     steps = x.walk!
     res = { ri:, ci:, steps: }
     results << res
   end
 end
+# pp results.map { |d| d[:steps]}.sort
 pp(results.count { |d| d[:steps] > 10_000 })
