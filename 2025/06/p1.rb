cfd0abbc1a4d4226e6b604c8fc19b04fa3b84a98
Kyle Soskin <37049625+kylesoskin@users.noreply.github.com>
2025-12-06 19:13:50 -0500

    p1

---
+++ 2025/06/p1.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+# input = 'sample_input.txt'
+input = 'input.txt'
+
+data = File.read(input).split("\n")
+flip = []
+data.each do |line|
+  split = line.split(' ')
+  split.each.with_index do |v, i|
+    flip[i] ||= []
+    flip[i] << v
+  end
+end
+def mult(d)
+  pp d
+  d.inject(1) { |prod, n| prod * n }
+end
+
+pp flip.map { |d|
+  op = d.pop
+  d.map!(&:to_i)
+  op == '+' ? d.sum : mult(d)
+}.sum
